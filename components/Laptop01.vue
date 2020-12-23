ab3a94058e22972ff2591cfa5c786b94c3537d19
Goran Milic <gmilic@gmail.com>
2020-12-23 15:30:41 +0100

    netlify-fix-2

---
+++ components/Laptop01.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+      <v-row>
+          <v-col>
+              <v-img
+                src="/img/Laptop01.png"
+              >
+              </v-img>
+          </v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+    name:"Laptop01"
+}
+</script>
+
+<style scoped>
+
+
+</style>
